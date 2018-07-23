a62af4135718e44e5fd356462cfb6224576a8b46
Sean McHenry <sean@browndesign.com>
2018-07-23 12:56:09 -0400

    Added initial plugin files

---
+++ includes/class-test-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://browndesign.com
+ * @since      1.0.0
+ *
+ * @package    Test_Plugin
+ * @subpackage Test_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Test_Plugin
+ * @subpackage Test_Plugin/includes
+ * @author     Sean McHenry <sean@browndesign.com>
+ */
+class Test_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-test-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://browndesign.com
+ * @since      1.0.0
+ *
+ * @package    Test_Plugin
+ * @subpackage Test_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Test_Plugin
+ * @subpackage Test_Plugin/includes
+ * @author     Sean McHenry <sean@browndesign.com>
+ */
+class Test_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
